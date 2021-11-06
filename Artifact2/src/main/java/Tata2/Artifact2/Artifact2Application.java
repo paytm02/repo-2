4f69742c481c7a3774903410ddd678fff14afd12
rakesh <raakhys@gmail.com>
2021-11-06 19:13:19 +0530

    list commit 2

---
+++ Artifact2/src/main/java/Tata2/Artifact2/Artifact2Application.java
@@ -0,0 +1,13 @@
+package Tata2.Artifact2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Artifact2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Artifact2Application.class, args);
+	}
+
+}
---
+++ Artifact2/src/test/java/Tata2/Artifact2/Artifact2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Tata2.Artifact2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Artifact2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
